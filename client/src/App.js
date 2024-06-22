03faed3b3840d0eeaeaf7c4a90e58c70d39bb3f4
Martin <martinsivak.15@gmail.com>
2024-06-22 22:36:58 +0200

    final version

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, { useState } from 'react';
-import {Button, Spinner} from 'react-bootstrap'
+import {Button, Spinner, Modal} from 'react-bootstrap'
 
 function App() {
 
-  const [two, setTwo] = useState(true)
-  const [three, setThree] = useState(true)
-  const [four, setFour] = useState(true)
-  const [five, setFive] = useState(true)
-  const [six, setSix] = useState(true)
-  const [seven, setSeven] = useState(true)
-  const [eight, setEight] = useState(true)
-  const [nine, setNine] = useState(true)
-  const [ten, setTen] = useState(true)
-  const [eleven, setEleven] = useState(true)
-  const [twelve, setTwelve] = useState(true)
+  const [two, setTwo] = useState(2)
+  const [three, setThree] = useState(3)
+  const [four, setFour] = useState(4)
+  const [five, setFive] = useState(5)
+  const [six, setSix] = useState(6)
+  const [seven, setSeven] = useState(7)
+  const [eight, setEight] = useState(8)
+  const [nine, setNine] = useState(9)
+  const [ten, setTen] = useState(10)
+  /*
+  const [eleven, setEleven] = useState(11)
+  const [twelve, setTwelve] = useState(12)
+  */
 
   const maxDiceValue = 6
   const minDiceValue = 1
@@ -25,7 +27,9 @@ function App() {
 
   const [rolling, setRolling] = useState(false)
 
-  const [currentValue, setCurrentValue] = useState(null)
+  const [currentValue, setCurrentValue] = useState(0)
+  const [hasPossibleCombination, setHasPossibleCombination] = useState(true);
+  const [score, setScore] = useState(0)
 
   const handleDiceRoll = () => {
     setRolling(true)
@@ -36,79 +40,163 @@ function App() {
       setDice1(result1)
       setDice2(result2)
 
-      setCurrentValue(result1 + result2)
+      let currentValue = result1 + result2
+      setCurrentValue(currentValue)
 
       setRolling(false)
-    }, 2000);
-  }
+
+      let numbers = []
+      if (two > 0) numbers.push(two);
+      if (three > 0) numbers.push(three);
+      if (four > 0) numbers.push(four);
+      if (five > 0) numbers.push(five);
+      if (six > 0) numbers.push(six);
+      if (seven > 0) numbers.push(seven);
+      if (eight > 0) numbers.push(eight);
+      if (nine > 0) numbers.push(nine);
+      if (ten > 0) numbers.push(ten);
+      /*
+      if (eleven > 0) numbers.push(eleven);
+      if (twelve > 0) numbers.push(twelve);
+      */
+
+      console.log(numbers)
+      console.log("Current value " + currentValue)
+
+    setTimeout(() => {
+
+      let combinations = [];
+      for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+          if (numbers[i] + numbers[j] === currentValue) {
+            combinations.push([numbers[i], numbers[j]]);
+          }
+        }
+      }
+
+      if (combinations.length > 0) {
+        setHasPossibleCombination(true);
+        console.log("Combination found")
+      } else if (numbers.includes(currentValue)) {
+        setHasPossibleCombination(true);
+        console.log("Combination found - same number")
+      } else {
+        setHasPossibleCombination(false);
+        console.log("No combination found")
+      }
+    }, 100);
+
+    
+  }, 400);
+}
+
+const handleNewGame = () => {
+  window.location.reload()
+}
 
   const handleSelectNumber = (number) => {
+    setScore(score + 1)
+    console.log(score)
+
     switch (number) {
       case 2:
         setCurrentValue(currentValue - 2);
-        setTwo(false);
+        setTwo(0);
         break
       case 3:
         setCurrentValue(currentValue - 3)
-        setThree(false)
+        setThree(0)
         break
       case 4:
         setCurrentValue(currentValue - 4)
-        setFour(false)
+        setFour(0)
         break
       case 5:
         setCurrentValue(currentValue - 5)
-        setFive(false)
+        setFive(0)
         break
       case 6:
         setCurrentValue(currentValue - 6)
-        setSix(false)
+        setSix(0)
         break
       case 7:
         setCurrentValue(currentValue - 7)
-        setSeven(false)
+        setSeven(0)
         break
       case 8:
         setCurrentValue(currentValue - 8)
-        setEight(false)
+        setEight(0)
         break
       case 9:
         setCurrentValue(currentValue - 9)
-        setNine(false)
+        setNine(0)
         break
       case 10:
         setCurrentValue(currentValue - 10)
-        setTen(false)
+        setTen(0)
         break
+        /*
       case 11:
         setCurrentValue(currentValue - 11)
-        setEleven(false)
+        setEleven(0)
         break
       case 12:
         setCurrentValue(currentValue - 12)
-        setTwelve(false)
+        setTwelve(0)
+        */
     }
   }
 
 
   return (
-    <div>
-        <Button variant={two ? "warning" : "success"} size="lg" disabled={currentValue < 2 || !two} onClick={() => handleSelectNumber(2)}>{two ? ("2") : ("✔")}</Button>
-        <Button variant={three ? "warning" : "success"} size="lg" disabled={currentValue < 3 || !three} onClick={() => handleSelectNumber(3)}>{three ? ("3") : ("✔")}</Button>
-        <Button variant={four ? "warning" : "success"} size="lg" disabled={currentValue < 4 || !four} onClick={() => handleSelectNumber(4)}>{four ? ("4") : ("✔")}</Button>
-        <Button variant={five ? "warning" : "success"} size="lg" disabled={currentValue < 5 || !five} onClick={() => handleSelectNumber(5)}>{five ? ("5") : ("✔")}</Button>
-        <Button variant={six ? "warning" : "success"} size="lg" disabled={currentValue < 6 || !six} onClick={() => handleSelectNumber(6)}>{six ? ("6") : ("✔")}</Button>
-        <Button variant={seven ? "warning" : "success"} size="lg" disabled={currentValue < 7 || !seven} onClick={() => handleSelectNumber(7)}>{seven ? ("7") : ("✔")}</Button>
-        <Button variant={eight ? "warning" : "success"} size="lg" disabled={currentValue < 8 || !eight} onClick={() => handleSelectNumber(8)}>{eight ? ("8") : ("✔")}</Button>
-        <Button variant={nine ? "warning" : "success"} size="lg" disabled={currentValue < 9 || !nine} onClick={() => handleSelectNumber(9)}>{nine ? ("9") : ("✔")}</Button>
-        <Button variant={ten ? "warning" : "success"} size="lg" disabled={currentValue < 10 || !ten} onClick={() => handleSelectNumber(10)}>{ten ? ("10") : ("✔")}</Button>
-        <Button variant={eleven ? "warning" : "success"} size="lg" disabled={currentValue < 11 || !eleven} onClick={() => handleSelectNumber(11)}>{eleven ? ("11") : ("✔")}</Button>
-        <Button variant={twelve ? "warning" : "success"} size="lg" disabled={currentValue < 12 || !twelve} onClick={() => handleSelectNumber(12)}>{twelve ? ("12") : ("✔")}</Button>
+    <div className="App">
+      <h1>SHUT THE BOX!</h1>
+      <div className="Content">
+      <div className="NumberButtons">
+        <Button className="NumberButton" variant={two ? "warning" : "success"} size="lg" disabled={currentValue < 2 || !two} onClick={() => handleSelectNumber(2)}>{two ? ("2") : ("✓")}</Button>
+        <Button className="NumberButton" variant={three ? "warning" : "success"} size="lg" disabled={currentValue < 3 || !three} onClick={() => handleSelectNumber(3)}>{three ? ("3") : ("✓")}</Button>
+        <Button className="NumberButton" variant={four ? "warning" : "success"} size="lg" disabled={currentValue < 4 || !four} onClick={() => handleSelectNumber(4)}>{four ? ("4") : ("✓")}</Button>
+        <Button className="NumberButton" variant={five ? "warning" : "success"} size="lg" disabled={currentValue < 5 || !five} onClick={() => handleSelectNumber(5)}>{five ? ("5") : ("✓")}</Button>
+        <Button className="NumberButton" variant={six ? "warning" : "success"} size="lg" disabled={currentValue < 6 || !six} onClick={() => handleSelectNumber(6)}>{six ? ("6") : ("✓")}</Button>
+        <Button className="NumberButton" variant={seven ? "warning" : "success"} size="lg" disabled={currentValue < 7 || !seven} onClick={() => handleSelectNumber(7)}>{seven ? ("7") : ("✓")}</Button>
+        <Button className="NumberButton" variant={eight ? "warning" : "success"} size="lg" disabled={currentValue < 8 || !eight} onClick={() => handleSelectNumber(8)}>{eight ? ("8") : ("✓")}</Button>
+        <Button className="NumberButton" variant={nine ? "warning" : "success"} size="lg" disabled={currentValue < 9 || !nine} onClick={() => handleSelectNumber(9)}>{nine ? ("9") : ("✓")}</Button>
+        <Button className="NumberButton" variant={ten ? "warning" : "success"} size="lg" disabled={currentValue < 10 || !ten} onClick={() => handleSelectNumber(10)}>{ten ? ("10") : ("✓")}</Button>
+        {/*<Button className="NumberButton" variant={eleven ? "warning" : "success"} size="lg" disabled={currentValue < 11 || !eleven} onClick={() => handleSelectNumber(11)}>{eleven ? ("11") : ("✓")}</Button>
+        <Button className="NumberButton" variant={twelve ? "warning" : "success"} size="lg" disabled={currentValue < 12 || !twelve} onClick={() => handleSelectNumber(12)}>{twelve ? ("12") : ("✓")}</Button>
+        */}
+        </div>
+      <div className="DiceButtons">
           <br/>
-        <Button variant="light">{rolling ? (<Spinner/>):(<h1>{dice1}</h1>)}</Button>
-        <Button variant="light">{rolling ? (<Spinner/>):(<h1>{dice2}</h1>)}</Button>
+        <Button className="DiceButton" variant="light" size="lg">{rolling ? (<Spinner/>):(<h2>{dice1}</h2>)}</Button>
+        <Button className="DiceButton" variant="light">{rolling ? (<Spinner/>):(<h2>{dice2}</h2>)}</Button>
         <br/>
-        <Button variant="info" disabled={!currentValue == 0} onClick={() => handleDiceRoll()}>Roll!</Button>
+      </div>
+        <Button className="RollButton" variant="info" disabled={currentValue !== 0} onClick={() => handleDiceRoll()}><h3>Roll!</h3></Button>
+      </div>
+
+        <Modal show={!hasPossibleCombination} centered className="Modal">
+        <Modal.Header as="h1">
+          <Modal.Title>You lost!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Score: {score}/9</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleNewGame} >
+            Play again!
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={two + three + four + five + six + seven + eight + nine + ten === 0} centered className="Modal">
+        <Modal.Header as="h1">
+          <Modal.Title>You won!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Congratulation! Score: {score}/9</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleNewGame} >
+            Play again!
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
